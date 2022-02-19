90fba1a1624cb2286d668640d44e4d8ca19b13e4
Memory-Wabwile <memorywabwile@gmail.com>
2022-02-19 12:06:55 +0300

    create a saveUser method to save all the users

--- user.py
+++ user.py
@@ -1,11 +1,12 @@
 # A user class to store user information
 class User 
 
-    all_users = []
+    all_Users[]
 
     def __innit__(self, username , password)
         self.username = username 
         self.password = password
 
-   
+    def saveUser(self)
+        User.all_Users.append(self)
         
