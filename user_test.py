23a766c90f0313a67133b69cd11dc08f3c024a6b
Memory-Wabwile <memorywabwile@gmail.com>
2022-02-19 15:31:03 +0300

    create a method of creating a new users username and storing it in username

--- user_test.py
+++ user_test.py
@@ -1,3 +1,21 @@
 import unittest
 from user import User
 
+class Test_User(unittest.TestCase) :
+
+    def create_account(self,username, password): 
+        if decision == new :
+            print("Enter Username")
+            username = input()
+            return True
+
+
+        while True :
+            print("Welcome to password Locker")
+            print("\n")
+            print("Type \"new\" to create a new account , \"log\" to log into an existing account or \"ex\" to exit")
+            decision = input().lower()
+            print("\n")
+
+if __name__=="__main__":
+    unittest.main()
