3b90adba611a99fa4167e34e8994c630b585c185
Memory-Wabwile <memorywabwile@gmail.com>
2022-02-19 16:37:26 +0300

    write a statement to enable user to choose from creating an account, logging in or exiting

--- user_test.py
+++ user_test.py
@@ -4,18 +4,21 @@ from user import User
 class Test_User(unittest.TestCase) :
 
     def create_account(self,username, password): 
-        if decision == new :
+        if decision == "new" :
             print("Enter Username")
-            username = input()
+            self.username = input()
             return True
+        else : return False 
 
-
-        while True :
-            print("Welcome to password Locker")
-            print("\n")
-            print("Type \"new\" to create a new account , \"log\" to log into an existing account or \"ex\" to exit")
-            decision = input().lower()
-            print("\n")
+    def exit(decision):
+        if decision == "ex": return True
+        else : return False
+            
+    while True :
+        print("Welcome to password Locker")
+        decision = input("Type \"new\" to create a new account , \"log\" to log into an existing account or \"ex\" to exit \n").lower()
+        print("\n")
+        if exit(decision):break
 
 if __name__=="__main__":
     unittest.main()
