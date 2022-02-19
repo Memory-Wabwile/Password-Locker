a203130a847307e5d6e12c8c5f508c66dfeb91f0
Memory-Wabwile <memorywabwile@gmail.com>
2022-02-19 13:02:18 +0300

    properly indent the code

--- user.py
+++ user.py
@@ -1,12 +1,12 @@
 # A user class to store user information
-class User 
+class User :
 
-    all_Users[]
+    all_Users = []
 
-    def __innit__(self, username , password)
+    def __innit__(self, username , password):
         self.username = username 
         self.password = password
 
-    def saveUser(self)
+    def saveUser(self):
         User.all_Users.append(self)
         
