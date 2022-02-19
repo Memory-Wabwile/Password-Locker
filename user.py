e82b88798d4947c77e88b4e9f705fb8553fff006
Memory-Wabwile <memorywabwile@gmail.com>
2022-02-19 11:46:45 +0300

    create a user file to store the users information

---
+++ user.py
@@ -0,0 +1,11 @@
+# A user class to store user information
+class User 
+
+    all_users = []
+
+    def __innit__(self, username , password)
+        self.username = username 
+        self.password = password
+
+   
+        
