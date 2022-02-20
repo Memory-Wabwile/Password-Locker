56a3999bf31f55442266d41a130d623041f160ef
Memory-Wabwile <memorywabwile@gmail.com>
2022-02-20 13:21:09 +0300

    verify the user if the username and password match

--- credentials.py
+++ credentials.py
@@ -4,11 +4,18 @@ class Credentials :
     '''
     credentials_list=[]
 
+    
     @classmethod
     def user_verification(cls , username , password):
         '''
-        method to verify if user is in the list
+        method to verify if user is in the all users list
         '''
+        verified_user=""
+        for user in User.all_users:
+            if(user.username == username  and user.password == password):
+                verified_user == user.username
+        return verified_user
+
 
     def __innit__(self,userName,passWord):
 
