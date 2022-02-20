22efb3d387195fda8ee2fc7046bf43ddec9fb64d
Memory-Wabwile <memorywabwile@gmail.com>
2022-02-20 19:25:30 +0300

    define a main method and print out a welcoming statement

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
 from user import User
+'''
+importing user details
+'''
 from credentials import Credentials
+'''
+importing the users credentials
+'''
+
+
+def create_new_user(username,password):
+    '''
+    method for creating new user
+    '''
+    new_user=(username,password)
+    return new_user
+
+
+
+def main():
+    print("Welcome to password Locker")
+
+if __name__ == '__main__' :
+    main()
