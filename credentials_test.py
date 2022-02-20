68f02f8bc3ce6ecf68523c0d823ef76179f11d31
Memory-Wabwile <memorywabwile@gmail.com>
2022-02-20 14:20:07 +0300

    indent the code

--- credentials_test.py
+++ credentials_test.py
@@ -13,6 +13,17 @@ class TestCredentials(unittest.TestCase):
         set up method to run before each test cases
         
         '''
-        self.new_user = Credentials('Instagram' , 'Memory' , '4321')   
+        self.new_user = Credentials('Instagram' , 'Memory' , '4321')  
 
 
+    def test__init__(self): 
+        '''
+        to test if the object is initialized properly
+        '''
+        self.assertEqual(self.new_credential.account,'Instagram')
+        self.assertEqual(self.new_credential.userName,'Memory')
+        self.assertEqual(self.new_credential.passWord,'4321')
+
+
+if __name__ == '__main__' :
+    unittest.main()
