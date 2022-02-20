f87f351e9ba39097a665069f71c919825372a108
Memory-Wabwile <memorywabwile@gmail.com>
2022-02-20 13:12:19 +0300

    create a method in credentials file to verify if user is in the usees list

--- credentials.py
+++ credentials.py
@@ -3,4 +3,14 @@ class Credentials :
     A credentials class to store all the user information and creation of new instances
     '''
     credentials_list=[]
+
+    @classmethod
+    def user_verification(cls , username , password):
+        '''
+        method to verify if user is in the list
+        '''
+
+    def __innit__(self,userName,passWord):
+
+
     
